3ff6f650a551953fded80bf1e43b76017c815965
Aryan <aryanpatil7050@gmail.com>
2026-03-31 08:19:31 +0530

    fix cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
